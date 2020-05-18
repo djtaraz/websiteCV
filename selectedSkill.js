eb6b855bc9f5f90300a4ca668bc95ba6dd8c8695
Ivan Taraszov <ivan.taraszov@city.ac.uk>
2020-05-18 16:04:21 +0300

    commuit new projects

--- selectedSkill.js
+++ selectedSkill.js
@@ -1,13 +1,13 @@
-var navContainer = document.getElementsByClassName("main");
+var btnContainer = document.getElementById("buttonDiv");
 
 // Get all buttons with class="btn" inside the container
-var navelement = btnContainer.getElementsByClassName("navelement");
+var buttonSkill = btnContainer.getElementsByClassName("buttonSkill");
 
 // Loop through the buttons and add the active class to the current/clicked button
-for (var i = 0; i < navelement.length; i++) {
-    navelement[i].addEventListener("click", function() {
-        var current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace(" active", "");
-        this.className += " active";
+for (var i = 0; i < buttonSkill.length; i++) {
+    buttonSkill[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("pressed");
+        current[0].className = current[0].className.replace(" pressed", "");
+        this.className += "pressed";
     });
 }
